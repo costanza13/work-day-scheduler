affe9156000a6892b03c5cc73b93ac8cece81a97
mcc <costanza+git@gmail.com>
2021-04-20 13:44:14 -0700

    implemenmt click-to-edit descriptions, save button; tweak styles

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,6 @@
 // initialize some globals
-const workdayFirstHour = 0;  // hour of day, 0 - 23
-const workdayLastHour = 23;  // hour of day, 0 - 23
+const workdayFirstHour = 8;  // hour of day, 0 - 23
+const workdayLastHour = 18;  // hour of day, 0 - 23
 var today = dayjs().startOf('day').format();  // date/timestamp for today at 00:00
 var now = dayjs();
 var currentHour = parseInt(dayjs().format('H'));
@@ -24,7 +24,7 @@ const ord = function() {
 var loadScheduleData = function () {
   // load workday schedule from local storage
   var scheduleJson = localStorage.getItem('schedule');
-  var scheduleData;
+  var scheduleData = null;
 
   if (scheduleJson) {
     scheduleData = JSON.parse(scheduleJson);
@@ -35,7 +35,7 @@ var loadScheduleData = function () {
     }
   }
 
-  if (scheduleJson === null) {
+  if (scheduleData === null) {
     scheduleData = {
       date: today,
       schedule: []
@@ -44,7 +44,7 @@ var loadScheduleData = function () {
     for (var hour = workdayFirstHour; hour <= workdayLastHour; hour++) {
       timeBLock = {
         hour: hour,
-        description: 'to-dos here'
+        description: ''
       };
       scheduleData.schedule.push(timeBLock);
     }
@@ -63,15 +63,8 @@ var buildScheduleEl = function(scheduleData) {
     var timeBlock = scheduleData.schedule[i];
     // console.log('timeBlock', timeBlock);
 
-    var descBgClass = 'present';
-    if (timeBlock.hour > currentHour) {
-      descBgClass = 'future';
-    } else if (timeBlock.hour < currentHour) {
-      descBgClass = 'past'
-    }
-
     var hourEl = $('<div>').attr('class', 'col-1 text-right hour');
-    var descriptionEl = $('<div>').attr('class', 'col-10 text-left description ' + descBgClass);
+    var descriptionEl = $('<div>').attr('class', 'col-10 text-left description past');
     var saveEl = $('<div>').attr('class', 'col-1 text-center saveBtn');
     hourEl.html(timeBlock.hour > 12 ? (timeBlock.hour - 12) + 'PM' : timeBlock.hour + 'AM');
     descriptionEl.html(timeBlock.description);
@@ -81,9 +74,7 @@ var buildScheduleEl = function(scheduleData) {
     $('#schedule').append(timeBlockEl);
   }
 
-  // add editable description handler
-  $('.present, .future').on('click', editDescription);
-
+  refreshScheduleStatuses();
   $('#schedule').fadeIn(1000); // show it when done building
 }
 
@@ -91,59 +82,96 @@ var buildScheduleEl = function(scheduleData) {
 var refreshScheduleStatuses = function() {
   var hourNow = parseInt(dayjs().format('H'));
 
-  // only if the hour has changed
-  if (hourNow !== currentHour) {
+  // at midnight, refresh everything
+  if (dayjs().startOf('day').format() !== today) {
+    clearInterval(heartbeat);
+    today = dayjs().startOf('day').format();
+    initSchedule();
 
-    // at midnight, refresh everything
-    if (dayjs().startOf('day').format() !== today) {
-      clearInterval(heartbeat);
-      today = dayjs().startOf('day').format();
-      initSchedule();
+  } else {
+    var timeBlocks = $('.time-block');
+    timeBlocks.each(function() {
+      var description = $(this).children('.description').first();
+      var blockHour = parseInt($(this).attr('data-hour'));
 
-    } else {
-      var timeBlocks = $('.time-block');
-      timeBlocks.each(function() {
-        var description = $(this).children('.description').first();
-        var blockHour = parseInt($(this).attr('data-hour'));
-
-        if (description.hasClass('present')) {
-          // probably have to change to 'past', so make sure
-          if (blockHour < hourNow) {
-            // yup, change to 'past'
-            description.removeClass('present');
-            description.addClass('past');
-          }
-        } else if (description.hasClass('future')) {
-          // might have to change to 'present', so check
-          if (blockHour === hourNow) {
-            // yup, change to 'present'
-            description.removeClass('future');
-            description.addClass('present');
-          } else if (blockHour < hourNow) {
-            // shouldn't happen, but cover it anyway
-            description.removeClass('future');
-            description.addClass('past');
-          }
+      if (description.hasClass('present')) {
+        // probably have to change to 'past', so make sure
+        if (blockHour < hourNow) {
+          // yup, change to 'past'
+          description.removeClass('present');
+          description.addClass('past');
         }
-      });
-    }
-
-    currentHour = hourNow;
+      } else if (description.hasClass('future')) {
+        // might have to change to 'present', so check
+        if (blockHour === hourNow) {
+          // yup, change to 'present'
+          description.removeClass('future');
+          description.addClass('present');
+        } else if (blockHour < hourNow) {
+          // shouldn't happen, but cover it anyway
+          description.removeClass('future');
+          description.addClass('past');
+        }
+      } else {
+        // might have to change from 'past', so check
+        if (blockHour === hourNow) {
+          description.removeClass('past');
+          description.addClass('present');
+        } else if (blockHour > hourNow) {
+          description.removeClass('past');
+          description.addClass('future');
+        }
+      }
+    });
   }
+
+  currentHour = hourNow;
+
+  // add editable description handler
+  $('.description').off('click');
+  $('.present, .future').on('click', editDescription);
 };
 
 var editDescription = function() {
-  console.log('description element:', $(this).html());
+  $(this).addClass('active');
   $(this).siblings('.saveBtn').addClass('active');
+  $(this).off('click');
+
+  var description = $(this).html();
+  console.log('description:', description);
+  var descriptionTextarea = $('<textarea class="edit-description">').val(description);
+  $(this).html(descriptionTextarea);
+  $('.edit-description').trigger('focus');
   
   // add save button handler
-  $('.active').on('click', saveDescription).css('cursor: pointer');
+  $('.saveBtn.active').on('click', saveDescription).css('cursor: pointer');
 };
 
 var saveDescription = function() {
   console.log('save button element', $(this).html());
   $(this).off('click');
   $(this).removeClass('active').css('cursor: default');
+
+  var timeBlockEl = $(this).parent();
+  var hour = parseInt(timeBlockEl.attr('data-hour'));
+  var descriptionEl = timeBlockEl.children('.description').first();
+  var editDescriptionEl = descriptionEl.children('.edit-description').first();
+  var newDescription = editDescriptionEl.val();
+  descriptionEl.html(newDescription);
+  descriptionEl.removeClass('active');
+  console.log('changing hour', hour);
+  console.log('changing to "' + newDescription + '"');
+
+  for (var i = 0; i < workdayScheduleData.schedule.length; i++) {
+    if (workdayScheduleData.schedule[i].hour === hour) {
+      console.log(workdayScheduleData.schedule[i].description);
+      workdayScheduleData.schedule[i].description = newDescription;
+      console.log(workdayScheduleData.schedule[i].description);
+      break;
+    }
+  }
+  localStorage.setItem('schedule', JSON.stringify(workdayScheduleData));
+  refreshScheduleStatuses();
 };
 
 var initSchedule = function() {
@@ -153,7 +181,8 @@ var initSchedule = function() {
 }
 
 
+// get things started
 initSchedule();
 
-
+// check for hour transitions and update past/present/future statuses
 var heartbeat = setInterval(refreshScheduleStatuses, 1000 * 60);
